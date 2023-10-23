cfec82005eddaa82aeaec073b27d450341158a38
doooox <dtopic12@gmail.com>
2023-10-24 00:30:39 +0200

    fix

--- apps/porfolio-be/src/service/config/db.ts
+++ apps/porfolio-be/src/service/config/db.ts
@@ -1,11 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   const dbName = process.env.NX_MONGO_URI;
   try {
-    await mongoose.connect(dbName);
+    await mongoose.connect(dbName, {
+      useUnifiedTopology: true,
+    } as any);
+    console.log('Connected to MongoDB');
   } catch (error) {
-    console.log(error);
+    console.error('Error connecting to MongoDB:', error);
     process.exit(1);
   }
 };
