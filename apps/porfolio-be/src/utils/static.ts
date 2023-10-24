b27a4760151ec7e8ec5e280407dd99bd0103f145
doooox <dtopic12@gmail.com>
2023-10-24 08:58:03 +0200

    fix

--- apps/porfolio-be/src/utils/static.ts
+++ apps/porfolio-be/src/utils/static.ts
@@ -2,7 +2,6 @@ import { CorsOptions } from 'cors';
 
 export const corsOptions: CorsOptions = {
   origin: 'https://dusantopic.onrender.com',
-  credentials: true,
   preflightContinue: true,
   optionsSuccessStatus: 200,
 };
